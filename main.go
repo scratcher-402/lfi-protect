683a11a2402b673d7e07aa437c0c8e35e31b91fd
scratcher402 <lonanon4828@gmail.com>
2025-11-04 08:35:10 +0300

    Configuration stuff

--- main.go
+++ main.go
@@ -2,14 +2,37 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+
+	"gopkg.in/yaml.v3"
 		)
 
+type Config struct {
+	Proxy ProxyConfig `yaml:"proxy"`
+}
+type ProxyConfig struct {
+	ListenAddr string `yaml:"listen"`
+	ServerAddr string `yaml:"server"`
+}
+
 func main() {
-	fmt.Println("Hello Goida")
-	proxy, err := NewProxy("http://localhost:8999/")
+	configFile, err := os.ReadFile("config.yaml")
 	if (err != nil) {
-		fmt.Println("Ошибка созлания прокси")
+		fmt.Println("Ошибка чтения конфига", err)
 		return
 	}
-	http.ListenAndServe(":8000", proxy)
+	var config Config
+	err = yaml.Unmarshal(configFile, &config)
+	if (err != nil) {
+		fmt.Println("Ошибка парсинга", err)
+	}
+	fmt.Println("Scr-LFI-Protect")
+	fmt.Println("Проксируем:", config.Proxy.ServerAddr)
+	fmt.Println("Прокси работает на", config.Proxy.ListenAddr)
+	proxy, err := NewProxy(config.Proxy.ServerAddr)
+	if (err != nil) {
+		fmt.Println("Ошибка создания прокси")
+		return
+	}
+	http.ListenAndServe(config.Proxy.ListenAddr, proxy)
 	}
